e539e63ca49698d38b33a7ecb0e674c755f81d91
Jeff Chavez <jeffchavez8588@gmail.com>
2014-11-19 13:18:19 -0800

    added drink view

---
+++ GN_Music_SDK_iOS/DrinkView.h
@@ -0,0 +1,16 @@
+//
+//  DrinkView.h
+//  Musicholic
+//
+//  Created by Jeff Chavez on 11/19/14.
+//  Copyright (c) 2014 CodeFellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinkView : UIView
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *labelView;
+
+@end
